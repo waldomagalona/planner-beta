009f5a49045dfd2cfced0682ae08c1a08a9bec4c
waldomagalona <waldomagalona@gmail.com>
2021-10-23 09:20:13 +0800

    fix validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   has_many :tasks
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
+  validates :name, presence: true, uniqueness: {case_sensitive: false, message: "Category name has already been used"}, length: { maximum: 100 }
   validates :description, presence:true, length: { maximum: 2000 }
 end
